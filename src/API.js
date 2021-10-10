9aa04a4a6a1b899c99d0bbfba7c3e78e03cb5434
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2021-10-10 18:25:17 -0500

    Fixed POST method, now its sending name, id and actual date

--- src/API.js
+++ src/API.js
@@ -8,6 +8,17 @@ function sendJSON_(jsonResponse) {
 }
 
 
+function compareTwoArray_(arr1, arr2) {
+  if(arr1.length !== arr2.length) return false;
+
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+
 function doGet() {
   const data = worksheet.getRange('A1').getDataRegion().getValues();
   const headers = data.shift();
@@ -29,8 +40,23 @@ function doGet() {
 
 
 function doPost(e){
+  let jsonResponse;
+  //Getting the spreadsheet's column names to ensure sending data matches
+  const headers = worksheet.getRange(1, 1, 1, worksheet.getLastColumn()).getValues()[0];
+  const headersOriginalOrder = headers.slice();
+  headers.sort();
+  //Check if incoming json matches with headers names
   const body = e.postData.contents;
   const bodyJSON = JSON.parse(body);
+  const headerPassed = Object.keys(bodyJSON).sort();
+
+  if(!compareTwoArray_(headers, headerPassed)) {
+    jsonResponse = {status: 500, message: 'Invalid arguments passed'};
+
+    return sendJSON_(jsonResponse);
+  }
+
+  const dataArray = headersOriginalOrder.map(header => bodyJSON[header]);
   
-  worksheet.appendRow( [bodyJSON.Nombre, bodyJSON.Identificacion, bodyJSON.Fecha] );
+  worksheet.appendRow(dataArray);
 }
