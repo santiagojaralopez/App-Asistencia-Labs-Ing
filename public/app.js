8e57e639f9dbd82a60217813c0f5d1d665602823
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2021-10-10 15:07:03 -0500

    Added promises to fetch

--- public/app.js
+++ public/app.js
@@ -4,23 +4,31 @@ const nombre = document.getElementById('nombre');
 const identificacion = document.getElementById('identificacion');
 const submitButton = document.getElementById('submit-button');
 
-submitButton.addEventListener("click", () => {
-    let userName = nombre.value;
-    let userId = identificacion.value;
-    let actualDate = new Date();
+submitButton.addEventListener("click", (e) => {
+  //e.preventDefault();
 
-    fetch(URL, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        redirect: 'follow', // manual, *follow, error
-        body: JSON.stringify({ 
-            "Nombre": userName,
-            "Identificacion": userId,
-            "Fecha": actualDate
-        }) // body data type must match "Content-Type" header
-      });
+  let data = {
+    "Nombre": nombre.value,
+    "Identificacion": identificacion.value,
+    "Fecha": new Date()
+  };
+
+  fetch(URL, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'no-cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+      body: JSON.stringify( data ) // body data type must match "Content-Type" header
+    }
+  )
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 });
